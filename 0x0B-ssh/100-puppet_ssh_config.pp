590789e3d1f5198a65d5970e64661326af6e4014
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-03-24 17:24:15 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -13,6 +13,6 @@ file_line { 'Turn off passwd auth':
 
 file_line { 'Declare identity file':
    path   => '/etc/ssh/ssh_config',
-   line   => 'IdentifyFile ~/.ssh/school',
-   match  => '^#IdentifyFile',
+   line   => 'IdentityFile ~/.ssh/school',
+   match  => '^#IdentityFile',
 }
