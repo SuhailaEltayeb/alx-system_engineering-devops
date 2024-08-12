a4303d2315809f34765f53fc9edc0e01b0c1f042
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-08-12 11:43:35 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-'''0-subs.py'''
+"""0-sub.py module"""
 
 
 def number_of_subscribers(subreddit):
-    '''Function to query the REDDIT API and return #
-    of subscripers for given subressit'''
+    """function to Query Reddit API and returns # of subscribers
+    to the subreddit"""
     import requests
 
-     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-     if sub_info.status_code >= 300:
-         return 0
+    if sub_info.status_code >= 300:
+        return 0
 
-     return sub_info.json().get("data").get("subscribers")
+    return sub_info.json().get("data").get("subscribers")
